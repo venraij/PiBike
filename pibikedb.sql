e4d2c0cfbe77e27b9d658bb66545db62299d48c1
venraij <45543766+venraij@users.noreply.github.com>
2019-09-09 12:27:19 +0200

    Add files via upload

---
+++ pibikedb.sql
@@ -0,0 +1,31 @@
+-- creates clean database for weerstation 
+DROP USER IF EXISTS 'sensem'@'localhost';
+DROP USER IF EXISTS 'senser'@'localhost';
+DROP DATABASE IF EXISTS pibike;
+CREATE DATABASE pibike;
+USE pibike;
+CREATE TABLE sensor (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  naam VARCHAR(45),
+  eenheid VARCHAR(45),
+  PRIMARY KEY (id)
+); 
+CREATE TABLE meting (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  sensor_id INT(11) NOT NULL,
+  tijd TIMESTAMP,
+  waarde FLOAT DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY fk_meting_sensor (sensor_id),
+  CONSTRAINT fk_meting_sensor FOREIGN KEY (sensor_id) REFERENCES sensor (id)
+); 
+CREATE USER 'sensem'@'localhost' IDENTIFIED BY 'h@';
+CREATE USER 'senser'@'localhost' IDENTIFIED BY 'h@';
+GRANT INSERT ON pibike.meting TO 'sensem'@'localhost';
+GRANT SELECT ON pibike.sensor TO 'sensem'@'localhost';
+GRANT SELECT ON pibike.* TO 'senser'@'localhost';
+
+INSERT INTO sensor (naam, eenheid) VALUES ('Lengtegraad', 'DD');
+INSERT INTO sensor (naam, eenheid) VALUES ('Breedtegraad', 'DD');
+
+

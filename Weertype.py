33041b2833412b2e73354e8c291dce5ff5b0e2ea
Standeee <55086170+Standeee@users.noreply.github.com>
2019-09-09 18:39:12 +0200

    Update Weertype.py

--- Weertype.py
+++ Weertype.py
@@ -1,5 +1,5 @@
 import urllib.request, json
-from sense_hat import SenseHat
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 from time import sleep
 
 # Define some colours
@@ -10,7 +10,8 @@ W = (255, 255, 255) # White
 E = (0, 0, 0) # Empty
 
 sense = SenseHat()
-delay = 5
+delay_img = 2
+delay_press = 5
 
 # Demo url to test, so the key doesn't expire
 demo_url = "http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam"
@@ -30,124 +31,129 @@ y = x[0]
 weertype = y['image']
 temp = y['temp']
 
-if weertype == "bewolkt" or weertype == "halfbewolkt" or weertype == "zwaarbewolkt" or weertype == "wolkennacht":
-# Set up where the image displays
-    bewolkt = [
-        G, G, E, E, E, E, E, G,
-        G, G, G, E, E, E, G, G,
-        E, E, E, E, E, E, E, E,
-        E, E, G, G, E, G, E, E,
-        E, G, G, G, G, G, G, E,
-        G, G, G, G, G, G, G, G,
-        G, G, G, G, G, G, G, G,
-        E, E, E, E, E, E, E, E
-    ]
+# A def so you can call it when you press a button
+def press(event):
+    if event.action != ACTION_RELEASED:
+        if weertype == "bewolkt" or weertype == "halfbewolkt" or weertype == "zwaarbewolkt" or weertype == "wolkennacht":
+        # Set up where the image displays
+            bewolkt = [
+                G, G, E, E, E, E, E, G,
+                G, G, G, E, E, E, G, G,
+                E, E, E, E, E, E, E, E,
+                E, E, G, G, E, G, E, E,
+                E, G, G, G, G, G, G, E,
+                G, G, G, G, G, G, G, G,
+                G, G, G, G, G, G, G, G,
+                E, E, E, E, E, E, E, E
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(bewolkt)
+        # Display these colours on the LED matrix
+            sense.set_pixels(bewolkt)
 
-elif weertype == "zonnig":
-# Set up where the image displays
-    zonnig = [
-        Y, E, Y, E, Y, E, Y, E,
-        E, E, E, E, E, E, E, Y,
-        Y, E, E, Y, Y, E, E, E,
-        E, E, Y, Y, Y, Y, E, Y,
-        Y, E, Y, Y, Y, Y, E, E,
-        E, E, E, Y, Y, E, E, Y,
-        Y, E, E, E, E, E, E, E,
-        E, Y, E, Y, E, Y, E, Y
-    ]
+        elif weertype == "zonnig":
+        # Set up where the image displays
+            zonnig = [
+                Y, E, Y, E, Y, E, Y, E,
+                E, E, E, E, E, E, E, Y,
+                Y, E, E, Y, Y, E, E, E,
+                E, E, Y, Y, Y, Y, E, Y,
+                Y, E, Y, Y, Y, Y, E, E,
+                E, E, E, Y, Y, E, E, Y,
+                Y, E, E, E, E, E, E, E,
+                E, Y, E, Y, E, Y, E, Y
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(zonnig)
+        # Display these colours on the LED matrix
+            sense.set_pixels(zonnig)
 
-elif weertype == "bliksem":
-# Set up where the image displays
-    bliksem = [
-        G, G, E, E, E, E, E, G,
-        B, G, G, E, E, E, G, B,
-        B, E, E, E, E, E, E, B,
-        E, E, G, G, E, G, E, E,
-        E, G, G, G, G, G, G, E,
-        G, G, Y, G, G, G, Y, G,
-        E, Y, Y, G, G, Y, Y, G,
-        E, Y, E, E, E, Y, E, E
-    ]
+        elif weertype == "bliksem":
+        # Set up where the image displays
+            bliksem = [
+                G, G, E, E, E, E, E, G,
+                B, G, G, E, E, E, G, B,
+                B, E, E, E, E, E, E, B,
+                E, E, G, G, E, G, E, E,
+                E, G, G, G, G, G, G, E,
+                G, G, Y, G, G, G, Y, G,
+                E, Y, Y, G, G, Y, Y, G,
+                E, Y, E, E, E, Y, E, E
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(bliksem)
+        # Display these colours on the LED matrix
+            sense.set_pixels(bliksem)
 
-elif weertype == "regen" or weertype == "buien":
-# Set up where the image displays
-    regen = [
-        G, G, E, E, E, E, E, G,
-        B, G, G, E, E, E, G, B,
-        B, E, E, E, E, E, E, B,
-        E, E, G, G, E, G, E, E,
-        E, G, G, G, G, G, G, E,
-        G, G, G, G, G, G, G, G,
-        B, G, B, G, B, G, B, G,
-        B, E, B, E, B, E, B, E
-    ]
+        elif weertype == "regen" or weertype == "buien":
+        # Set up where the image displays
+            regen = [
+                G, G, E, E, E, E, E, G,
+                B, G, G, E, E, E, G, B,
+                B, E, E, E, E, E, E, B,
+                E, E, G, G, E, G, E, E,
+                E, G, G, G, G, G, G, E,
+                G, G, G, G, G, G, G, G,
+                B, G, B, G, B, G, B, G,
+                B, E, B, E, B, E, B, E
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(regen)
+        # Display these colours on the LED matrix
+            sense.set_pixels(regen)
 
-elif weertype == "hagel" or weertype == "sneeuw":
-# Set up where the image displays
-    sneeuw = [
-        E, E, G, G, E, G, E, E,
-        E, G, G, G, G, G, G, E,
-        G, G, G, G, G, G, G, G,
-        E, W, E, W, E, W, E, W,
-        W, E, W, E, W, E, W, E,
-        E, W, E, W, E, W, E, W,
-        W, E, W, E, W, E, W, E,
-        E, W, E, W, E, W, E, W
-    ]
+        elif weertype == "hagel" or weertype == "sneeuw":
+        # Set up where the image displays
+            sneeuw = [
+                E, E, G, G, E, G, E, E,
+                E, G, G, G, G, G, G, E,
+                G, G, G, G, G, G, G, G,
+                E, W, E, W, E, W, E, W,
+                W, E, W, E, W, E, W, E,
+                E, W, E, W, E, W, E, W,
+                W, E, W, E, W, E, W, E,
+                E, W, E, W, E, W, E, W
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(sneeuw)
+        # Display these colours on the LED matrix
+            sense.set_pixels(sneeuw)
 
-elif weertype == "mist" or weertype == "nachtmist":
-# Set up where the image displays
-    mist = [
-        E, E, E, E, E, E, E, E,
-        E, E, E, E, E, E, E, E,
-        E, E, E, E, E, E, E, E,
-        E, E, E, E, E, E, E, E,
-        E, E, E, E, E, E, E, E,
-        E, G, G, E, G, G, G, E,
-        G, G, G, G, G, G, G, G,
-        G, G, G, G, G, G, G, G
-    ]
+        elif weertype == "mist" or weertype == "nachtmist":
+        # Set up where the image displays
+            mist = [
+                E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E,
+                E, G, G, E, G, G, G, E,
+                G, G, G, G, G, G, G, G,
+                G, G, G, G, G, G, G, G
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(mist)
+        # Display these colours on the LED matrix
+            sense.set_pixels(mist)
 
-elif weertype == "helderenacht":
-# Set up where the image displays
-    nacht = [
-        E, E, E, E, E, E, E, E,
-        E, E, E, G, G, E, E, E,
-        E, E, G, G, G, G, E, E,
-        E, G, G, G, G, G, G, E,
-        E, G, G, G, G, G, G, E,
-        E, E, G, G, G, G, E, E,
-        E, E, E, G, G, E, E, E,
-        E, E, E, E, E, E, E, E,
-    ]
+        elif weertype == "helderenacht":
+        # Set up where the image displays
+            nacht = [
+                E, E, E, E, E, E, E, E,
+                E, E, E, G, G, E, E, E,
+                E, E, G, G, G, G, E, E,
+                E, G, G, G, G, G, G, E,
+                E, G, G, G, G, G, G, E,
+                E, E, G, G, G, G, E, E,
+                E, E, E, G, G, E, E, E,
+                E, E, E, E, E, E, E, E,
+            ]
 
-# Display these colours on the LED matrix
-    sense.set_pixels(nacht)
+        # Display these colours on the LED matrix
+            sense.set_pixels(nacht)
 
-else:
-# Yeah Earth is gone...
-    print("There is no weather, Earth is gone")
+        else:
+        # Yeah Earth is gone...
+            print("There is no weather, Earth is gone")
 
-sleep(delay)
-sense.clear()
+        sleep(delay_img)
+        sense.clear()
+        sense.show_message(temp)
+        sleep(delay_press)
 
-# Shows tempature
-sense.show_message(temp)
+# If middle button is pressed then it will show weather and tempature
+sense.stick.direction_middle = press

db5476c9cf5f75f99fc08e5a9de691b959c8ed96
Standeee <55086170+Standeee@users.noreply.github.com>
2019-09-10 17:49:03 +0200

    Update weer.py

--- weer.py
+++ weer.py
@@ -1,6 +1,75 @@
 import urllib.request, json
 from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 from time import sleep
+import sys, getopt
+import argparse
+import time
+import mysql.connector as mariadb
+from mysql.connector import errorcode
+
+sensor_name = "Temperatuur"
+sensor_name1 = "Image"
+
+# database connection configuration
+dbconfig = {
+    'user': 'sensem',
+    'password': 'h@',
+    'host': 'localhost',
+    'database': 'pibike',
+    'raise_on_warnings': True,
+}
+
+# parse arguments
+verbose = True
+interval = 10   # second
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "vt:")
+except getopt.GetoptError as err:
+    print(str(err))
+    print('measure.py -v -t <interval>')
+    print('-v: be verbose')
+    print('-t <interval>: measure each <interval> seconds (default: 10s)')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-v':
+        verbose = True
+    elif opt == '-t':
+        interval = int(arg)
+        
+#verbinden met de database        
+try:
+    mariadb_connection = mariadb.connect(**dbconfig)
+    if verbose:
+        print("Database connected")
+except mariadb.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print("Error: {}".format(err))
+    sys.exit(2)
+    
+# create the database cursor for executing SQL queries
+cursor = mariadb_connection.cursor()
+# turn on autocommit
+#cursor.autocommit = True
+
+# determine the latest lat
+try:
+    cursor.execute('SELECT waarde FROM meting WHERE sensor_id = 1 ORDER BY id DESC LIMIT 1;')
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+charlie = cursor.fetchone()
+
+# determine the latest long
+try:
+    cursor.execute('SELECT waarde FROM meting WHERE sensor_id = 2 ORDER BY id DESC LIMIT 1;')
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+delta = cursor.fetchone()
 
 # Define some colours
 Y = (255, 247, 0) # Yellow
@@ -19,17 +88,16 @@ basis_url = "http://weerlive.nl/api/json-data-10min.php?key="
 # Key invullen
 key = "27a31f3737"
 rest_url = "&locatie="
-lat = 52
-long = 5
 
 # Change (demo_url) to (basis_url + key + rest_url + str(lat) + "," + str(long)) for normal code
-response = urllib.request.urlopen(basis_url + key + rest_url + str(lat) + "," + str(long))
+response = urllib.request.urlopen(basis_url + key + rest_url + str(charlie[0]) + "," + str(delta[0]))
 data = json.loads(response.read().decode("utf-8"))
 x = data['liveweer']
 y = x[0]
 
-weertype = y['image']
+image = y['image']
 temp = y['temp']
+plaats = y['plaats']
 
 # A def so you can call it when you press a button
 def press(event):
@@ -168,8 +236,92 @@ def press(event):
 
         sleep(delay_img)
         sense.clear()
-        sense.show_message(temp + "C", text_colour=[255, 255, 0])
+        sense.show_message(t + "C", text_colour=[255, 255, 0])
         sleep(delay_press)
 
 # If middle button is pressed then it will show weather and tempature
 sense.stick.direction_middle = press
+
+try:
+    cursor.execute("SELECT id FROM sensor WHERE naam=%s", [sensor_name])
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+sensor_id = cursor.fetchone()
+if sensor_id == None:
+    print("Error: no sensor found with naam = %s" % sensor_name)
+    sys.exit(2)
+if verbose:
+    print("Reading data from sensor %s with id %s" % (sensor_name, sensor_id[0]))
+        
+#Temperatuur
+t = temp
+        
+# verbose
+if verbose:
+    print("Temperature: %s C" % t)
+    # store measurement in database
+    try:
+        cursor.execute('INSERT INTO meting (waarde, sensor_id) VALUES (%s, %s);', (t, sensor_id[0]))
+    except mariadb.connector.Error as err:
+        print("Error: {}".format(err))
+    else:
+        # commit measurements
+        mariadb_connection.commit();
+if verbose:
+        print("Temperature committed");
+            
+# determine the sensor_id for image
+try:
+    cursor.execute("SELECT id FROM sensor WHERE naam=%s", [sensor_name1])
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+sensor_id = cursor.fetchone()
+if sensor_id == None:
+    print("Error: no sensor found with naam = %s" % sensor_name1)
+    sys.exit(2)
+if verbose:
+    print("Reading data from sensor %s with id %s" % (sensor_name1, sensor_id[0]))
+        
+#weertype
+weertype = image
+        
+# verbose
+if verbose:
+    print("Image: %s" % image)
+    # store measurement in database
+    try:
+        cursor.execute('INSERT INTO meting (weertype, sensor_id) VALUES (%s, %s);', (image, sensor_id[0]))
+    except mariadb.connector.Error as err:
+        print("Error: {}".format(err))
+    else:
+        # commit measurements
+        mariadb_connection.commit();
+if verbose:
+    print("Image committed");
+
+# determine the latest temperature
+try:
+    cursor.execute('SELECT waarde FROM meting WHERE sensor_id = 3 ORDER BY id DESC LIMIT 1;')
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+alfa = cursor.fetchone()
+print(alfa[0])
+
+# determine the latest weertype
+try:
+    cursor.execute('SELECT weertype FROM meting WHERE sensor_id = 4 ORDER BY id DESC LIMIT 1;')
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+bravo = cursor.fetchone()
+print(bravo[0])
+
+print(plaats)
+
+# close db connection
+mariadb_connection.close()
+# done
+

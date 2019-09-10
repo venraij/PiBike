f05677fecf381543ed91fcd72a9cfde11b23770c
Robin K <Slagpit@hotmail.com>
2019-09-10 14:05:49 +0200

    Falldetect 1.0
    needs tuning, accelerometer Z axis comparison commented out

--- py/falldetect.py
+++ py/falldetect.py
@@ -3,22 +3,18 @@
 # Imports
 from sense_hat import SenseHat
 import time
-# debug
 
 # Variables
-wait_interval = 0.5
-
-# Declare sense hat 
+wait_interval = 1
 sense = SenseHat()
 fallen = False
 debug = False
+# define ranges for orientation sensor that equals fall
+orient_min = 85
+orient_max = 285
 
-# Booleans IMU Compass / Gyro / Accel. More sensors = more accurate
+# Booleans IMU - Compass / Gyro / Accel. More sensors = more accurate
 sense.set_imu_config(True, True, True)
-
-# Redundant, formatting use only
-#accel_data = sense.get_accelerometer()
-#accel_data_raw = sense.get_accelerometer_raw()
 
 
 # Function Declerations
@@ -27,9 +23,6 @@ def orientation_watchdog():
     
     # Returns Orientation - pitch , roll , yaw.
     # Only roll being relevant
-    # Range left: 0° - 90°
-    # Range Right: 360° - 270°
-    # Difference 90°!
     # type float
     sense_orientation = sense.get_orientation()
 
@@ -39,23 +32,23 @@ def orientation_watchdog():
     # type float
     sense_accelleration = sense.get_accelerometer_raw()
 
-    # TODO
-    # if range outside values Left or Right - throw "fallen" else - False.
-    # Also , show message as feedback on Sense HAT LED
 
     # First, fetch me the specific sensor values I need
     roll = sense_orientation["roll"]
     z_accel = sense_accelleration["z"]
 
+    # Variables defined up top for easy reference
+    global orient_plus
+    global orient_minus
+    global fallen
 
-
-    # Calculate whether roll between 90 - 270
+        # Calculate whether roll between 90 - 270
     # Calculate whether force in Z axis sufficient enough to constitute a fall
-    if roll > 90 and roll < 270 and z_accel > 1 or z_accel < 0:
-        print("fall value")
-        print(roll)
-    
-    
+    # and (z_accel > 1 or z_accel < 0) 
+    if roll > orient_min and roll < orient_max:
+        fallen = True
+    else:
+        fallen = False
 
     # Debug loop to help me log measurement blocks
     global debug
@@ -63,12 +56,15 @@ def orientation_watchdog():
         print("begin meetsessie")
         debug = True
 
-    #debug - print sensor values    
+    #debug - print sensor values
+        
     #orientation 
     #print("p: {pitch}, r: {roll}, y: {yaw}".format(**sense_orientation))
+    #print(roll)
     #accelerometer
-    print(z_accel)
+    print(str(z_accel) + " vallen staat op " + str(fallen))
     #print("x: {x}, y: {y}, z: {z}".format(**sense_accelleration))
+ 
 
 # Main Loop
 try:
